83017136b4d422ef44bde2051c26b435f699e5b0
aiisiik <ai.sen@mail.ru>
2022-09-28 19:59:05 +0600

    Добавьте файлы проекта.

---
+++ String1/String1.cpp
@@ -0,0 +1,11 @@
+﻿#include <string>
+#include <iostream>
+
+int main()
+{
+    std::string s = "Let's go to Kazakhstan";
+    std::cout << s <<"\n";
+    std::cout << s.length() << "\n";
+    std::cout << s[0] << "\n";
+    std::cout << s[s.length()-1] << "\n";
+}
